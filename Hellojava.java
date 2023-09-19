997d53671d2448d8827677fe45a3e703b18c163a
Emitta2 <145447703+Emitta2@users.noreply.github.com>
2023-09-19 16:23:08 +0530

    Add files via upload

---
+++ Hellojava.java
@@ -0,0 +1,16 @@
+/*
+ * File       :Hellojava.java
+ * Description:My First Java Program "Hello Java"
+ * Author     :Emitta Mathew
+ * Version    :1.0
+ * Date       :18/09/23
+ *              
+ */
+package javalab;
+
+public class Hellojava {
+	public static void main(String[] args) {
+	System.out.println("Hello Java");
+    System.out.print("Hello Java");
+}
+}
