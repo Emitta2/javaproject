d0ffa0a0f54a517aca3d35d1ff27890a12d4e042
Emitta2 <145447703+Emitta2@users.noreply.github.com>
2023-10-20 15:05:43 +0530

    Add files via upload

---
+++ abstraction.java
@@ -0,0 +1,31 @@
+package javalab;
+
+public class abstraction {
+	public static void main(String [] args) {
+		
+		Rectangle rectangle=new Rectangle();
+		Triangle triangle=new Triangle();
+		Hexagon hexagon=new Hexagon();
+	    rectangle.numberOfSides();
+	    triangle.numberOfSides();
+	    hexagon.numberOfSides();
+	}
+}
+abstract class Shapes{
+	abstract public void numberOfSides();	
+}
+  class Rectangle extends Shapes{
+	  public void numberOfSides() {
+	  System.out.println("The number of sides of rectangle is 4");
+	  }
+  }
+  class Triangle extends Shapes{
+	  public void numberOfSides() {
+		  System.out.println("The number of sides of triangle is 3");
+	  }
+  }
+  class Hexagon extends Shapes{
+	  public void numberOfSides() {
+		  System.out.println("The number of sides of hexagon is 6");
+	  }
+  }
